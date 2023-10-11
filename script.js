0572ef4b982fe24863ffb098e7e67c69fffee7a2
Aditya-2027 <aditya.nandakumar17@gmail.com>
2023-10-11 20:17:48 +0530

    Version 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Get a reference to the button element
+const showMessageButton = document.getElementById("showMessageButton");
+
+// Add a click event listener to the button
+showMessageButton.addEventListener("click", () => {
+    // Display a message in an alert box
+    alert("Hello! Thanks for visiting my portfolio website.");
+});
